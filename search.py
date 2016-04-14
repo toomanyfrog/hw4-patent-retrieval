d6f4177dd24f1cb49a79baff0061d567d2d05981
Yui Wei <tyuiwei@gmail.com>
2016-04-14 23:57:39 +0800

    update lib

--- search.py
+++ search.py
@@ -3,6 +3,7 @@ import sys
 import xml.etree.ElementTree as ET
 import nltk
 import string
+import gensim
 from nltk.stem.porter import *
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
